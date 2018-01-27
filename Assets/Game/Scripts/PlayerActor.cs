c5d419352e5cd37b0ee1f08b55f229108a972742
Josue Soto <jsoto@headlesschickengames.com>
2018-01-27 11:01:04 -0600

    Agarrar y reparar la maquina

--- Assets/Game/Scripts/PlayerActor.cs
+++ Assets/Game/Scripts/PlayerActor.cs
@@ -13,23 +13,53 @@ public class PlayerActor : MonoBehaviour
 	private float movementSpeed, lifeSecondCounter;
 	private Vector3 movement;
 	private int originalLifeCount;
+	private bool isDead = true;
 
 	private Rigidbody rbody;
+	public Transform grabbedObj;
+	private Transform formerParent;
 
+	public LayerMask mask;
 
 	// Use this for initialization
 	void Start () 
 	{
 		rbody = GetComponent<Rigidbody> ();
 		originalLifeCount = playerLifes;
+		ownBase.actor = this;
+
+		RespawnCharacter();
 	}
 
 	private void Update ()
 	{
-		if (inputIndex >= 0) {
+		if (inputIndex >= 0) 
+		{
 			movementSpeed = Input.GetKey ("joystick " + (inputIndex + 1) + " button 0") ? runSpeed : moveSpeed;
-		} else {
+
+			// Grab things
+			if(Input.GetKeyDown("joystick " + (inputIndex + 1) + " button 6"))
+			{
+				GrabObject();
+			}
+			else if(Input.GetKeyUp("joystick " + (inputIndex + 1) + " button 6"))
+			{
+				ReleaseObject();
+			}
+		} 
+		else 
+		{
 			movementSpeed = Input.GetKey (KeyCode.LeftShift) ? runSpeed : moveSpeed;
+
+			// Grab things
+			if(Input.GetKeyDown(KeyCode.X))
+			{
+				GrabObject();
+			}
+			else if(Input.GetKeyUp(KeyCode.X))
+			{
+				ReleaseObject();
+			}
 		}
 	}
 	
@@ -44,11 +74,14 @@ public class PlayerActor : MonoBehaviour
 							   vertical * movementSpeed);
 
 		rbody.velocity = movement;
+
+		if(horizontal != 0 || vertical != 0)
+			transform.rotation = Quaternion.LookRotation(new Vector3(horizontal, 0, vertical));
 	}
 
 	private void OnTriggerStay (Collider other)
 	{
-		if(other.name.ToLower().Contains("heal") && other.transform.IsChildOf(ownBase.transform))
+		if(other.name.ToLower().Contains("heal") && other.transform.IsChildOf(ownBase.transform) && ownBase.isWorking)
 		{
 			if (Time.time > lifeSecondCounter) 
 			{
@@ -60,4 +93,59 @@ public class PlayerActor : MonoBehaviour
 			}
 		}
 	}
+
+	private void OnCollisionEnter (Collision other)
+	{
+		if(other.collider.gameObject.name.Contains(ownBase.name) && isDead)
+		{
+			for(int i = 0; i < ownBase.batteries.Count; i++)
+			{
+				ownBase.batteries[i].GetComponent<Rigidbody>().AddExplosionForce(250, transform.position, 50);
+			}
+			isDead = false;
+		}
+	}
+
+	public void RespawnCharacter ()
+	{
+		isDead = true;
+
+
+		Vector3 localBase = transform.InverseTransformDirection(ownBase.transform.position);
+		localBase.y += 4;
+		transform.rotation = Quaternion.LookRotation(-ownBase.transform.up);
+		localBase += transform.forward.normalized;
+
+		transform.localPosition = localBase;
+	}
+
+	private void GrabObject ()
+	{
+		if(grabbedObj == null)
+		{
+			Debug.Log("Try");
+			Collider[] cols = Physics.OverlapSphere(transform.position, 3, mask);
+			if(cols.Length > 0)
+			{
+				grabbedObj = cols[0].transform;
+				cols[0].isTrigger = true;
+				grabbedObj.GetComponent<Rigidbody>().isKinematic = true;
+				formerParent = grabbedObj.parent;
+				grabbedObj.SetParent(transform);
+			}
+		}
+	}
+
+	public void ReleaseObject ()
+	{
+		if(grabbedObj != null)
+		{
+			grabbedObj.GetComponent<Collider>().isTrigger = false;
+			grabbedObj.GetComponent<Rigidbody>().isKinematic = false;
+			grabbedObj.SetParent(formerParent);
+			formerParent = null;
+			grabbedObj = null;
+		}
+	}
+
 }
--- Assets/Game/Scripts/PlayerBase.cs
+++ Assets/Game/Scripts/PlayerBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using matnesis.TeaTime;
 
 public class PlayerBase : MonoBehaviour 
 {
@@ -8,37 +9,73 @@ public class PlayerBase : MonoBehaviour
 
 	public List<GameObject> batteries = new List<GameObject>();
 	public bool[] batteriesOn;
+	public PlayerActor actor;
 
+	private Vector3[] batteriesSnapPos;
+	private Quaternion[] batteriesSnapRot;
 	private int batteryLoopCount;
 
 	// Use this for initialization
 	public void Start () 
 	{
 		batteriesOn = new bool[batteries.Count];
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-//		batteryLoopCount = 0;
-//		for(int i = 0; i < batteries.Count; i++)
-//		{
-//			if(batte)
-//		}
+		batteriesSnapPos = new Vector3[batteries.Count];
+		batteriesSnapRot = new Quaternion[batteries.Count];
+
+		for(int i = 0; i < batteries.Count; i++)
+		{
+			batteriesSnapPos[i] = batteries[i].transform.position;
+			batteriesSnapRot[i] = batteries[i].transform.rotation;
+		}
+
+
+
+		this.tt ("@CheckIfIsWorking").Add (() => 
+		{
+			bool working = true;
+			for (int i = 0; i < batteries.Count; i++) 
+			{
+				if (batteriesOn [i] == false) 
+				{
+					working = false;
+				}
+			}
+			isWorking = working;
+		}).Add (0.2f).Repeat ();
 	}
 
-	private void OnTriggerStay (Collider other)
+	private void OnTriggerEnter (Collider other)
 	{
 		for(int i = 0; i < batteries.Count; i++)
 		{
 			if(batteries.Contains(other.gameObject))
 			{
 				int batteryIndex = batteries.IndexOf (other.gameObject);
+
 				batteriesOn [batteryIndex] = true;
 				batteries [batteryIndex].GetComponent<Renderer> ().materials [1].color = Color.green;
+				batteries[batteryIndex].transform.position = batteriesSnapPos[batteryIndex];
+				batteries[batteryIndex].transform.rotation = batteriesSnapRot[batteryIndex];
+				actor.ReleaseObject();
 			}
 		}
 	}
+
+//	private void OnTriggerStay (Collider other)
+//	{
+//		for(int i = 0; i < batteries.Count; i++)
+//		{
+//			if(batteries.Contains(other.gameObject))
+//			{
+//				if(!batteriesOn[batteries.IndexOf (other.gameObject)])
+//				{
+//					int batteryIndex = batteries.IndexOf (other.gameObject);
+//					batteriesOn [batteryIndex] = true;
+//					batteries [batteryIndex].GetComponent<Renderer> ().materials [1].color = Color.green;
+//				}
+//			}
+//		}
+//	}
 
 	private void OnTriggerExit (Collider other)
 	{
