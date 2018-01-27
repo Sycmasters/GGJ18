e7bd49ebdbc3d5878b15e96a7b7686cdb6d7f3a2
Josue Soto <jsoto@headlesschickengames.com>
2018-01-27 00:08:07 -0600

    Prototipando la escena con pantalla dividida

---
+++ Assets/Game/Scripts/CameraBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour 
+{
+	public Camera playerOneCam, playerTwoCam, playerThreeCam, playerFourCam;
+
+	// Use this for initialization
+	private void Start () 
+	{
+		//Debug.Log();
+		playerOneCam.rect = new Rect(0, .5f, .5f, .5f);
+		playerTwoCam.rect = new Rect(.5f, .5f, .5f, .5f);
+		playerThreeCam.rect = new Rect(0, 0, .5f, .5f);
+		playerFourCam.rect = new Rect(.5f, 0, .5f, .5f);
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		if(Input.GetKey(KeyCode.H))
+		{
+			playerOneCam.rect = new Rect (0, 0f, .5f, 1);
+			playerThreeCam.gameObject.SetActive(false);
+		}
+		else
+		{
+			//Debug.Log();
+			playerOneCam.rect = new Rect(0, .5f, .5f, .5f);
+			playerThreeCam.rect = new Rect(0f, 0, .5f, .5f);
+			playerThreeCam.gameObject.SetActive (true);
+		}
+		if (Input.GetKey (KeyCode.K)) 
+		{
+			playerOneCam.rect = new Rect (0, 0, 1, 1);
+			playerTwoCam.gameObject.SetActive (false);
+			playerThreeCam.gameObject.SetActive (false);
+			playerFourCam.depth = 0;
+		}
+		else
+		{
+			playerOneCam.rect = new Rect(0, .5f, .5f, .5f);
+			playerTwoCam.gameObject.SetActive (true);
+			playerThreeCam.gameObject.SetActive (true);
+			playerFourCam.depth = -1;
+		}
+	}
+}
